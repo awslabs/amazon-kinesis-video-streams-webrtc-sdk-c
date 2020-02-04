29efe7b14cfbfa97e513e5eb33c1fa9c553f21e9
Sean DuBois <seaduboi@amazon.com>
2020-02-03 16:09:25 -0800

    Properly parse compound RTCP packets
    Before we only parsed the first message in the datagram
    
    Resolves #45

--- src/source/PeerConnection/Rtcp.c
+++ src/source/PeerConnection/Rtcp.c
@@ -2,21 +2,27 @@
 
 #include "../Include_i.h"
 
-STATUS onRtcpPacket(PKvsPeerConnection pKvsPeerConnection, PBYTE pBuff, INT32 pBuffLen)
+STATUS onRtcpPacket(PKvsPeerConnection pKvsPeerConnection, PBYTE pBuff, UINT32 buffLen)
 {
     STATUS retStatus = STATUS_SUCCESS;
     RtcpPacket rtcpPacket;
+    UINT32 currentOffset = 0;
 
     CHK(pKvsPeerConnection != NULL && pBuff != NULL, STATUS_NULL_ARG);
-
     MEMSET(&rtcpPacket, 0x00, SIZEOF(rtcpPacket));
-    CHK_STATUS(setRtcpPacketFromBytes(pBuff, pBuffLen, &rtcpPacket));
 
-    if (rtcpPacket.header.packetType == RTCP_PACKET_TYPE_GENERIC_RTP_FEEDBACK
-        && rtcpPacket.header.receptionReportCount == RTCP_FEEDBACK_MESSAGE_TYPE_NACK) {
-        CHK_STATUS(resendPacketOnNack(&rtcpPacket, pKvsPeerConnection));
+    while (currentOffset < buffLen) {
+        CHK_STATUS(setRtcpPacketFromBytes(pBuff + currentOffset, buffLen - currentOffset, &rtcpPacket));
+
+        if (rtcpPacket.header.packetType == RTCP_PACKET_TYPE_GENERIC_RTP_FEEDBACK && rtcpPacket.header.receptionReportCount == RTCP_FEEDBACK_MESSAGE_TYPE_NACK) {
+            CHK_STATUS(resendPacketOnNack(&rtcpPacket, pKvsPeerConnection));
+        }
+
+        currentOffset += (rtcpPacket.payloadLength + RTCP_PACKET_HEADER_LEN);
     }
+
 CleanUp:
+    CHK_LOG_ERR_NV(retStatus);
 
     return retStatus;
 }
