f95963cf16c4f746f737111beb30cdcd8aa54584
Alex Zhukov <alex.zhukov@uber.com>
2020-04-08 19:48:39 -0700

    oops

--- src/source/Ice/IceAgentStateMachine.c
+++ src/source/Ice/IceAgentStateMachine.c
@@ -200,14 +200,12 @@ STATUS executeNewIceAgentState(UINT64 customData, UINT64 time)
     UNUSED_PARAM(time);
     STATUS retStatus = STATUS_SUCCESS;
     PIceAgent pIceAgent = (PIceAgent) customData;
-    BOOL locked = FALSE;
+    //assumes this is called while holding pIceAgent->lock
+    BOOL locked = TRUE;
 
     CHK(pIceAgent != NULL, STATUS_NULL_ARG);
     // return early if we are already in ICE_AGENT_STATE_GATHERING
     CHK(pIceAgent->iceAgentState != ICE_AGENT_STATE_NEW, retStatus);
-
-    //assumes this is called while holding pIceAgent->lock
-    locked = TRUE;
 
     pIceAgent->iceAgentState = ICE_AGENT_STATE_NEW;
 
