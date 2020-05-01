779e5a98983a775d40e2f1fd5383c5cd885b0865
Alex Zhukov <alex.zhukov@uber.com>
2020-05-01 14:53:21 -0700

    handle three video tracks

--- src/source/Sdp/Serialize.c
+++ src/source/Sdp/Serialize.c
@@ -6,11 +6,14 @@ STATUS parseMediaName(PSessionDescription pSessionDescription, PCHAR pch, UINT32
     ENTERS();
     STATUS retStatus = STATUS_SUCCESS;
 
+    CHK(pSessionDescription->mediaCount <= MAX_SDP_SESSION_MEDIA_COUNT, STATUS_BUFFER_TOO_SMALL);
+
     STRNCPY(pSessionDescription->mediaDescriptions[pSessionDescription->mediaCount].mediaName,
             (pch + SDP_ATTRIBUTE_LENGTH),
             MIN(MAX_SDP_MEDIA_NAME_LENGTH, lineLen - (SDP_ATTRIBUTE_LENGTH - 1)));
     pSessionDescription->mediaCount++;
 
+CleanUp:
     LEAVES();
     return retStatus;
 }
