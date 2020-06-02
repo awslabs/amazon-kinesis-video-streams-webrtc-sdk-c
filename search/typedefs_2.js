ac536b5831195b2ad2e24b7483fcbd24495efd75
Travis CI User <travis@example.org>
2020-06-02 19:21:56 +0000

    Auto-generated from travis

--- search/typedefs_2.js
+++ search/typedefs_2.js
@@ -3,10 +3,10 @@ var searchData=
   ['rtcdatachannel_691',['RtcDataChannel',['../Include_8h.html#aa6f47a9f98e9103afc97434dd3a5cd8c',1,'Include.h']]],
   ['rtconbandwidthestimation_692',['RtcOnBandwidthEstimation',['../Include_8h.html#af77d88f0ced976d3628f86b79ba0b868',1,'Include.h']]],
   ['rtconconnectionstatechange_693',['RtcOnConnectionStateChange',['../Include_8h.html#ad3f715dac4dd4d756e1728a37fb46eb9',1,'Include.h']]],
-  ['rtcondatachannel_694',['RtcOnDataChannel',['../Include_8h.html#a750b71698b51cb05fec761921c64c6f9',1,'Include.h']]],
+  ['rtcondatachannel_694',['RtcOnDataChannel',['../Include_8h.html#a264cd66b2d9905c3085cab6f3faf650b',1,'Include.h']]],
   ['rtconframe_695',['RtcOnFrame',['../Include_8h.html#a6269176a5be7e2e91fec6b067f7b9c5b',1,'Include.h']]],
   ['rtconicecandidate_696',['RtcOnIceCandidate',['../Include_8h.html#abad4874074cecf3de44c82498437355a',1,'Include.h']]],
-  ['rtconmessage_697',['RtcOnMessage',['../Include_8h.html#a9076ac4f31a29ed413ad20bd31c5cfde',1,'Include.h']]],
-  ['rtconopen_698',['RtcOnOpen',['../Include_8h.html#a148ed1868bd8ec813749211f5c87b98a',1,'Include.h']]],
+  ['rtconmessage_697',['RtcOnMessage',['../Include_8h.html#a19016a711a2550a8544e6c86c7f45d0f',1,'Include.h']]],
+  ['rtconopen_698',['RtcOnOpen',['../Include_8h.html#a0c9cbf4449a44e9a3847be0ef53b22a8',1,'Include.h']]],
   ['rtconpictureloss_699',['RtcOnPictureLoss',['../Include_8h.html#ac7f4225ac2545664aabdc88d0b58e329',1,'Include.h']]]
 ];
