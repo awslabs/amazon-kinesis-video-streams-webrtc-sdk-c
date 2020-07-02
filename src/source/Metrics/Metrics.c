a14b13a6006e77d36211165d686772ba5f782bd4
Alex Zhukov <alex.zhukov@uber.com>
2020-07-02 16:09:46 -0700

    clang format

--- src/source/Metrics/Metrics.c
+++ src/source/Metrics/Metrics.c
@@ -4,12 +4,14 @@
 #define LOG_CLASS "Metrics"
 #include "../Include_i.h"
 
-STATUS getIceCandidatePairStats(PRtcPeerConnection pRtcPeerConnection, PRtcIceCandidatePairStats pRtcIceCandidatePairStats)
+STATUS getIceCandidatePairStats(PRtcPeerConnection pRtcPeerConnection,
+                                PRtcIceCandidatePairStats pRtcIceCandidatePairStats)
 {
     STATUS retStatus = STATUS_SUCCESS;
     PKvsPeerConnection pKvsPeerConnection = (PKvsPeerConnection) pRtcPeerConnection;
     CHK((pRtcPeerConnection != NULL || pRtcIceCandidatePairStats != NULL), STATUS_NULL_ARG);
-    pRtcIceCandidatePairStats->requestsReceived = pKvsPeerConnection->pIceAgent->rtcIceMetrics.rtcIceCandidatePairStats.requestsReceived;
+    pRtcIceCandidatePairStats->requestsReceived =
+        pKvsPeerConnection->pIceAgent->rtcIceMetrics.rtcIceCandidatePairStats.requestsReceived;
 CleanUp:
     return retStatus;
 }
@@ -47,7 +49,8 @@ CleanUp:
     return retStatus;
 }
 
-STATUS getRtpRemoteInboundStats(PRtcPeerConnection pRtcPeerConnection, PRtcRemoteInboundRtpStreamStats pRtcRemoteInboundRtpStreamStats)
+STATUS getRtpRemoteInboundStats(PRtcPeerConnection pRtcPeerConnection,
+                                PRtcRemoteInboundRtpStreamStats pRtcRemoteInboundRtpStreamStats)
 {
     STATUS retStatus = STATUS_SUCCESS;
     PKvsPeerConnection pKvsPeerConnection = (PKvsPeerConnection) pRtcPeerConnection;
@@ -69,7 +72,8 @@ CleanUp:
     return retStatus;
 }
 
-STATUS logWebRTCMetrics(PRtcPeerConnection pRtcPeerConnection, RTC_STATS_TYPE statsType) {
+STATUS logWebRTCMetrics(PRtcPeerConnection pRtcPeerConnection, RTC_STATS_TYPE statsType)
+{
     UNUSED_PARAM(statsType);
     STATUS retStatus = STATUS_SUCCESS;
     CHK(pRtcPeerConnection != NULL, STATUS_NULL_ARG);
@@ -82,43 +86,43 @@ STATUS rtcPeerConnectionGetMetrics(PRtcPeerConnection pRtcPeerConnection, PRtcSt
 {
     STATUS retStatus = STATUS_SUCCESS;
     CHK(pRtcPeerConnection != NULL || pRtcMetrics != NULL, STATUS_NULL_ARG);
-    switch(pRtcMetrics->requestedTypeOfStats) {
-    case RTC_STATS_TYPE_CANDIDATE_PAIR:
-        getIceCandidatePairStats(pRtcPeerConnection, &pRtcMetrics->rtcStatsObject.iceCandidatePairStats);
-        break;
-    case RTC_STATS_TYPE_LOCAL_CANDIDATE:
-        getIceCandidateStats(pRtcPeerConnection, &pRtcMetrics->rtcStatsObject.localIceCandidateStats);
-        break;
-    case RTC_STATS_TYPE_REMOTE_CANDIDATE:
-        getIceCandidateStats(pRtcPeerConnection, &pRtcMetrics->rtcStatsObject.remoteIceCandidateStats);
-        break;
-    case  RTC_STATS_TYPE_TRANSPORT:
-        getTransportStats(pRtcPeerConnection, &pRtcMetrics->rtcStatsObject.transportStats);
-        break;
-    case  RTC_STATS_TYPE_REMOTE_INBOUND_RTP:
-        getRtpRemoteInboundStats(pRtcPeerConnection, &pRtcMetrics->rtcStatsObject.remoteInboundRtpStreamStats);
-        break;
-    case  RTC_STATS_TYPE_OUTBOUND_RTP:
-        getRtpOutboundStats(pRtcPeerConnection, &pRtcMetrics->rtcStatsObject.outboundRtpStreamStats);
-        break;
-    case  RTC_STATS_TYPE_ICE_SERVER:
-        getIceServerStats(pRtcPeerConnection, &pRtcMetrics->rtcStatsObject.iceServerStats);
-        break;
-    case  RTC_STATS_TYPE_CERTIFICATE:
-    case  RTC_STATS_TYPE_CSRC:
-    case  RTC_STATS_TYPE_INBOUND_RTP:
-    case  RTC_STATS_TYPE_REMOTE_OUTBOUND_RTP:
-    case  RTC_STATS_TYPE_PEER_CONNECTION:
-    case  RTC_STATS_TYPE_DATA_CHANNEL:
-    case  RTC_STATS_TYPE_RECEIVER:
-    case  RTC_STATS_TYPE_SENDER:
-    case  RTC_STATS_TYPE_TRACK:
-    case  RTC_STATS_TYPE_CODEC:
-    case  RTC_STATS_TYPE_SCTP_TRANSPORT:
-    case  RTC_STATS_TYPE_TRANSCEIVER:
-    case  RTC_STATS_TYPE_RTC_ALL:
-    default:
-        CHK(FALSE, STATUS_NOT_IMPLEMENTED);
+    switch (pRtcMetrics->requestedTypeOfStats) {
+        case RTC_STATS_TYPE_CANDIDATE_PAIR:
+            getIceCandidatePairStats(pRtcPeerConnection, &pRtcMetrics->rtcStatsObject.iceCandidatePairStats);
+            break;
+        case RTC_STATS_TYPE_LOCAL_CANDIDATE:
+            getIceCandidateStats(pRtcPeerConnection, &pRtcMetrics->rtcStatsObject.localIceCandidateStats);
+            break;
+        case RTC_STATS_TYPE_REMOTE_CANDIDATE:
+            getIceCandidateStats(pRtcPeerConnection, &pRtcMetrics->rtcStatsObject.remoteIceCandidateStats);
+            break;
+        case RTC_STATS_TYPE_TRANSPORT:
+            getTransportStats(pRtcPeerConnection, &pRtcMetrics->rtcStatsObject.transportStats);
+            break;
+        case RTC_STATS_TYPE_REMOTE_INBOUND_RTP:
+            getRtpRemoteInboundStats(pRtcPeerConnection, &pRtcMetrics->rtcStatsObject.remoteInboundRtpStreamStats);
+            break;
+        case RTC_STATS_TYPE_OUTBOUND_RTP:
+            getRtpOutboundStats(pRtcPeerConnection, &pRtcMetrics->rtcStatsObject.outboundRtpStreamStats);
+            break;
+        case RTC_STATS_TYPE_ICE_SERVER:
+            getIceServerStats(pRtcPeerConnection, &pRtcMetrics->rtcStatsObject.iceServerStats);
+            break;
+        case RTC_STATS_TYPE_CERTIFICATE:
+        case RTC_STATS_TYPE_CSRC:
+        case RTC_STATS_TYPE_INBOUND_RTP:
+        case RTC_STATS_TYPE_REMOTE_OUTBOUND_RTP:
+        case RTC_STATS_TYPE_PEER_CONNECTION:
+        case RTC_STATS_TYPE_DATA_CHANNEL:
+        case RTC_STATS_TYPE_RECEIVER:
+        case RTC_STATS_TYPE_SENDER:
+        case RTC_STATS_TYPE_TRACK:
+        case RTC_STATS_TYPE_CODEC:
+        case RTC_STATS_TYPE_SCTP_TRANSPORT:
+        case RTC_STATS_TYPE_TRANSCEIVER:
+        case RTC_STATS_TYPE_RTC_ALL:
+        default:
+            CHK(FALSE, STATUS_NOT_IMPLEMENTED);
     }
 CleanUp:
     return retStatus;
