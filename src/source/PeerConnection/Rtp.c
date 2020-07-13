awslabs/amazon-kinesis-video-streams-webrtc-sdk-c

commit c93141c1c4d9992c9190727bf5a8377ac9f1c0b1

Add definition for freeTransceiver
We had a forward declaration, but no implementation. Return STATUS_NOT_IMPLEMENTED
since we don't support renegotiation yet, so users shouldn't be using this.

src/source/PeerConnection/Rtp.c
@@ -42,6 +42,11 @@ CleanUp:
     return retStatus;
 }
 
+STATUS freeTransceiver(PRtcRtpTransceiver *pRtcRtpTransceiver) {
+    UNUSED_PARAM(pRtcRtpTransceiver);
+    return STATUS_NOT_IMPLEMENTED;
+}
+
 STATUS freeKvsRtpTransceiver(PKvsRtpTransceiver* ppKvsRtpTransceiver)
 {
     STATUS retStatus = STATUS_SUCCESS;

src/include/com/amazonaws/kinesis/video/webrtcclient/Include.h
@@ -1561,6 +1561,9 @@ PUBLIC_API STATUS transceiverOnBandwidthEstimation(PRtcRtpTransceiver, UINT64, R
 /**
  * @brief Frees the previously created transceiver object
  *
+ * This method is currently a no-op as Transceivers are freed when freePeerConnection is called
+ * in the future when renegotiation is supported this will be useful to remove Transceivers at anytime
+ *
  * @param[in/out/opt] PRtcRtpTransceiver* IN/OUT/OPT RtcRtpTransceiver to be freed
  *
  * @return STATUS code of the execution. STATUS_SUCCESS on success

commit f8c37b2bb3360594dca4afb096721465b4665183

Fix clang-format error
Accidentally introduced by c93141c1c4d9992c9190727bf5a8377ac9f1c0b1

src/source/PeerConnection/Rtp.c
@@ -42,7 +42,8 @@ CleanUp:
     return retStatus;
 }
 
-STATUS freeTransceiver(PRtcRtpTransceiver *pRtcRtpTransceiver) {
+STATUS freeTransceiver(PRtcRtpTransceiver* pRtcRtpTransceiver)
+{
     UNUSED_PARAM(pRtcRtpTransceiver);
     return STATUS_NOT_IMPLEMENTED;
 }