66ef16fb53a978e42b167d471263ea5807ea05c4
Lukas Herman <lherman.cs@gmail.com>
2020-08-31 12:01:27 -0700

    Ignore fork builds for check-sample

--- check-sample.sh
+++ check-sample.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+if [[ -z "$AWS_ACCESS_KEY_ID" || -z "$AWS_SECRET_ACCESS_KEY" ]]
+then
+  echo "Couldn't find AWS credentials. Very likely this build is coming from a fork. Ignoring."
+  exit 0
+fi
+
 # Set bash to print out every command that's running to the screen
+# Set logging after checking credentials so that we don't leak them
 set -xv
 
 pwd
