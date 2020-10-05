17171f487a03d58b82e959121fd466c351ed665b
ycyang <yc.yang1229@gmail.com>
2020-10-06 04:04:41 +0800

    fix-sdp-version-deserialization. (#842)

--- src/source/Sdp/Deserialize.c
+++ src/source/Sdp/Deserialize.c
@@ -134,7 +134,7 @@ STATUS deserializeSessionDescription(PSessionDescription pSessionDescription, PC
             }
 
             if (0 == STRNCMP(curr, SDP_VERSION_MARKER, (ARRAY_SIZE(SDP_VERSION_MARKER) - 1))) {
-                STRTOUI64(curr, curr + MAX_SDP_TOKEN_LENGTH, 10, &pSessionDescription->version);
+                STRTOUI64(curr + SDP_ATTRIBUTE_LENGTH, curr + MIN(lineLen, MAX_SDP_TOKEN_LENGTH), 10, &pSessionDescription->version);
             }
 
             if (0 == STRNCMP(curr, SDP_ATTRIBUTE_MARKER, (ARRAY_SIZE(SDP_ATTRIBUTE_MARKER) - 1))) {
