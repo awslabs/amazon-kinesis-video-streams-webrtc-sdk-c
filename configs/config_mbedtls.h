c205f1ddd231f4c11f6be8b4a9b3f8e73890425f
Lukas Herman <lherman.cs@gmail.com>
2020-12-07 22:55:29 +0000

    Update mbedTLS config

--- configs/config_mbedtls.h
+++ configs/config_mbedtls.h
@@ -10,6 +10,15 @@ extern "C" {
 // enable DTLS-SRTP extension
 #define MBEDTLS_SSL_DTLS_SRTP
 
+// disable TLS 1.0 and 1.1 as they should be deprecated in major browser vendors
+#undef MBEDTLS_SSL_PROTO_TLS1
+#undef MBEDTLS_SSL_PROTO_TLS1_1
+#undef MBEDTLS_SSL_CBC_RECORD_SPLITTING
+
+// disable because they don't comply with AWS security standard
+#undef MBEDTLS_ECP_DP_SECP224K1_ENABLED
+#undef MBEDTLS_ECP_DP_SECP256K1_ENABLED
+
 #ifdef __cplusplus
 }
 #endif
