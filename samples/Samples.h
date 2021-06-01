fed35a25f6a2615f6227b97d809d8134e8e008ec
delaplan <delaplan@u48757f25b2c250.ant.amazon.com>
2021-06-01 12:34:41 -0700

    Added explanation of how to use IOT_CORE_ENABLE_CREDENTIALS

--- samples/Samples.h
+++ samples/Samples.h
@@ -45,6 +45,8 @@ extern "C" {
 #define IOT_CORE_PRIVATE_KEY         ((PCHAR) "AWS_IOT_CORE_PRIVATE_KEY")
 #define IOT_CORE_ROLE_ALIAS          ((PCHAR) "AWS_IOT_CORE_ROLE_ALIAS")
 #define IOT_CORE_THING_NAME          ((PCHAR) "AWS_IOT_CORE_THING_NAME")
+
+/* Uncomment the following line in order to enable IoT credentials checks in the provided samples */
 //#define IOT_CORE_ENABLE_CREDENTIALS  1
 
 typedef enum {
