08db299d1922685dd929afe2f1f0f2d82b8eda92
Travis CI User <travis@example.org>
2021-10-29 21:56:14 +0000

    Auto-generated from travis

--- search/classes_1.js
+++ search/classes_1.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['allocatesctpsortdatachannelsdata_2630',['AllocateSctpSortDataChannelsData',['../structAllocateSctpSortDataChannelsData.html',1,'']]]
+  ['allocatesctpsortdatachannelsdata_2626',['AllocateSctpSortDataChannelsData',['../structAllocateSctpSortDataChannelsData.html',1,'']]]
 ];
