eb07d741a5c0e4dc3a8c41208ccccc4684a37e05
Divya Sampath Kumar <disa6302@colorado.edu>
2021-12-07 11:51:11 -0800

    Rebase on develop and fix clang failure

--- src/include/com/amazonaws/kinesis/video/webrtcclient/Include.h
+++ src/include/com/amazonaws/kinesis/video/webrtcclient/Include.h
@@ -1192,7 +1192,7 @@ typedef struct {
                                                                //!< located. For default value or when file caching is not
                                                                //!< being used this value can be NULL or point to an EMPTY_STRING.
     KvsRetryStrategyCallbacks signalingRetryStrategyCallbacks; //!< Retry strategy callbacks used while creating signaling client
-    INT32 signalingClientCreationMaxRetryAttempts;             //!< Max attempts to create signaling client before returning error to the caller
+    UINT32 signalingClientCreationMaxRetryAttempts;            //!< Max attempts to create signaling client before returning error to the caller
     UINT32 stateMachineRetryCountReadOnly; //!< Retry count of state machine. Note that this **MUST NOT** be modified by the user. It is a read only
                                            //!< field
 } SignalingClientInfo, *PSignalingClientInfo;
