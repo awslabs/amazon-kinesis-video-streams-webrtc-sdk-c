1799f7c3e86e58bf1b6e125f7402d8b7e3029803
James Delaplane <delaplan@amazon.com>
2021-12-20 11:12:23 -0800

    update comment

--- src/source/Signaling/Signaling.c
+++ src/source/Signaling/Signaling.c
@@ -450,6 +450,8 @@ STATUS signalingFetchSync(PSignalingClient pSignalingClient)
     }
 
     // move to the fromGetToken() so we can move to the necessary step
+    // We start from get token to keep the design consistent with how it was when the constructor (create)
+    // would bring you to the READY state, but this is a two-way door and can be redone later.
     setStateMachineCurrentState(pSignalingClient->pStateMachine, SIGNALING_STATE_GET_TOKEN);
 
     //if we're not failing from a bad token, set the result to OK to that fromGetToken will move
