670b0bbc99b21bac25f2fc50438324bb6fd8fd7b
Hassan Sahibzada <hsahibza@amazon.com>
2022-02-08 08:57:01 -0500

    new Chrome v98 produces extra sdp attributes, up the limit so we do not reject

--- src/source/Sdp/Sdp.h
+++ src/source/Sdp/Sdp.h
@@ -75,7 +75,7 @@ extern "C" {
 #define MAX_SDP_SESSION_MEDIA_COUNT   5
 #define MAX_SDP_MEDIA_BANDWIDTH_COUNT 2
 
-#define MAX_SDP_ATTRIBUTES_COUNT 128
+#define MAX_SDP_ATTRIBUTES_COUNT 256
 
 /*
  * c=<nettype> <addrtype> <connection-address>
