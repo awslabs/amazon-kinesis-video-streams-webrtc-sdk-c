0fbddaa7116f1bbcb33b809b7b60759e40e7885b
Alex.D.Scofield <zhiqinli@amazon.com>
2022-05-11 06:24:20 +0800

    metrics: allow viewer sample to get ice candidate pair stats (#1406)
    Signed-off-by: Alex.Li <zhiqinli@amazon.com>

--- samples/Common.c
+++ samples/Common.c
@@ -1317,6 +1317,8 @@ STATUS signalingMessageReceived(UINT64 customData, PReceivedSignalingMessage pRe
                 // NULL the pointer to avoid it being freed in the cleanup
                 pPendingMessageQueue = NULL;
             }
+
+            startStats = pSampleConfiguration->iceCandidatePairStatsTimerId == MAX_UINT32;
             break;
 
         case SIGNALING_MESSAGE_TYPE_ICE_CANDIDATE:
