96855cad11955b71a8c41b92e917111009004f1a
Niyati Maheshwari <niyatim23@gmail.com>
2022-05-15 21:14:47 -0700

    add test

--- tst/SdpApiTest.cpp
+++ tst/SdpApiTest.cpp
@@ -1076,6 +1076,253 @@ a=rtpmap:102 H264/90000
     });
 }
 
+TEST_F(SdpApiTest, answerMlinesOrderSameAsOfferMLinesOrder)
+{
+    auto offer = std::string(R"(v=0
+o=- 481034601 1588366671 IN IP4 0.0.0.0
+s=-
+t=0 0
+a=fingerprint:sha-256 87:E6:EC:59:93:76:9F:42:7D:15:17:F6:8F:C4:29:AB:EA:3F:28:B6:DF:F8:14:2F:96:62:2F:16:98:F5:76:E5
+a=group:BUNDLE 0 1 2
+a=ice-options:trickle
+)");
+
+    auto sdpOfferAudio1 = R"(m=audio 9 UDP/TLS/RTP/SAVPF 109 9 0 8 101
+c=IN IP4 0.0.0.0
+a=recvonly
+a=extmap:1 urn:ietf:params:rtp-hdrext:ssrc-audio-level
+a=extmap:2/recvonly urn:ietf:params:rtp-hdrext:csrc-audio-level
+a=extmap:3 urn:ietf:params:rtp-hdrext:sdes:mid
+a=fmtp:109 maxplaybackrate=48000;stereo=1;useinbandfec=1
+a=fmtp:101 0-15
+a=ice-pwd:db2619f637ea75cf7e578e8fc7829ebf
+a=ice-ufrag:6a957b4a
+a=mid:1
+a=rtcp-mux
+a=rtpmap:109 opus/48000/2
+a=rtpmap:9 G722/8000/1
+a=rtpmap:0 PCMU/8000
+a=rtpmap:8 PCMA/8000
+a=rtpmap:101 telephone-event/8000
+a=setup:actpass
+a=ssrc:1326673407 cname:{080eadab-2d5f-4ba3-abc2-0ccc305a028d})";
+
+    auto sdpOfferVideo1 = R"(m=video 9 UDP/TLS/RTP/SAVPF 97 98 99 100 101 122 102 121 127 120 125 107 108 109 124 119 123
+c=IN IP4 0.0.0.0
+a=rtcp:9 IN IP4 0.0.0.0
+a=ice-ufrag:XvdX
+a=ice-pwd:VbwqLiodQFWAt3YVdyK/HG04
+a=ice-options:trickle
+a=fingerprint:sha-256 F0:6C:41:02:7D:AC:E0:CA:3B:2A:F9:92:F9:13:86:67:DA:71:5B:4E:E4:83:80:C5:87:3F:3B:4D:41:F2:91:44
+a=setup:actpass
+a=mid:0
+a=extmap:1 urn:ietf:params:rtp-hdrext:toffset
+a=extmap:2 http://www.webrtc.org/experiments/rtp-hdrext/abs-send-time
+a=extmap:3 urn:3gpp:video-orientation
+a=extmap:4 http://www.ietf.org/id/draft-holmer-rmcat-transport-wide-cc-extensions-01
+a=extmap:5 http://www.webrtc.org/experiments/rtp-hdrext/playout-delay
+a=extmap:6 http://www.webrtc.org/experiments/rtp-hdrext/video-content-type
+a=extmap:7 http://www.webrtc.org/experiments/rtp-hdrext/video-timing
+a=extmap:8 http://www.webrtc.org/experiments/rtp-hdrext/color-space
+a=extmap:9 urn:ietf:params:rtp-hdrext:sdes:mid
+a=extmap:10 urn:ietf:params:rtp-hdrext:sdes:rtp-stream-id
+a=extmap:11 urn:ietf:params:rtp-hdrext:sdes:repaired-rtp-stream-id
+a=recvonly
+a=rtcp-mux
+a=rtcp-rsize
+a=rtpmap:97 rtx/90000
+a=fmtp:97 apt=96
+a=rtpmap:98 VP9/90000
+a=rtcp-fb:98 goog-remb
+a=rtcp-fb:98 transport-cc
+a=rtcp-fb:98 ccm fir
+a=rtcp-fb:98 nack
+a=rtcp-fb:98 nack pli
+a=fmtp:98 profile-id=0
+a=rtpmap:99 rtx/90000
+a=fmtp:99 apt=98
+a=rtpmap:100 VP9/90000
+a=rtcp-fb:100 goog-remb
+a=rtcp-fb:100 transport-cc
+a=rtcp-fb:100 ccm fir
+a=rtcp-fb:100 nack
+a=rtcp-fb:100 nack pli
+a=fmtp:100 profile-id=2
+a=rtpmap:101 rtx/90000
+a=fmtp:101 apt=100
+a=rtpmap:122 VP9/90000
+a=rtcp-fb:122 goog-remb
+a=rtcp-fb:122 transport-cc
+a=rtcp-fb:122 ccm fir
+a=rtcp-fb:122 nack
+a=rtcp-fb:122 nack pli
+a=fmtp:122 profile-id=1
+a=rtpmap:102 H264/90000
+a=rtcp-fb:102 goog-remb
+a=rtcp-fb:102 transport-cc
+a=rtcp-fb:102 ccm fir
+a=rtcp-fb:102 nack
+a=rtcp-fb:102 nack pli
+a=fmtp:102 level-asymmetry-allowed=1;packetization-mode=1;profile-level-id=42001f
+a=rtpmap:121 rtx/90000
+a=fmtp:121 apt=102
+a=rtpmap:127 H264/90000
+a=rtcp-fb:127 goog-remb
+a=rtcp-fb:127 transport-cc
+a=rtcp-fb:127 ccm fir
+a=rtcp-fb:127 nack
+a=rtcp-fb:127 nack pli
+a=fmtp:127 level-asymmetry-allowed=1;packetization-mode=0;profile-level-id=42001f
+a=rtpmap:120 rtx/90000
+a=fmtp:120 apt=127
+a=rtpmap:125 H264/90000
+a=rtcp-fb:125 goog-remb
+a=rtcp-fb:125 transport-cc
+a=rtcp-fb:125 ccm fir
+a=rtcp-fb:125 nack
+a=rtcp-fb:125 nack pli
+a=fmtp:125 level-asymmetry-allowed=1;packetization-mode=1;profile-level-id=42e01f
+a=rtpmap:107 rtx/90000
+a=fmtp:107 apt=125
+a=rtpmap:108 H264/90000
+a=rtcp-fb:108 goog-remb
+a=rtcp-fb:108 transport-cc
+a=rtcp-fb:108 ccm fir
+a=rtcp-fb:108 nack
+a=rtcp-fb:108 nack pli
+a=fmtp:108 level-asymmetry-allowed=1;packetization-mode=0;profile-level-id=42e01f
+a=rtpmap:109 rtx/90000
+a=fmtp:109 apt=108
+a=rtpmap:124 red/90000
+a=rtpmap:119 rtx/90000
+a=fmtp:119 apt=124
+a=rtpmap:123 ulpfec/90000)";
+
+    auto sdpOfferVideo2 = R"(m=video 9 UDP/TLS/RTP/SAVPF 124 121 125 126 127 97 98
+c=IN IP4 0.0.0.0
+a=recvonly
+a=extmap:3 urn:ietf:params:rtp-hdrext:sdes:mid
+a=extmap:4 http://www.webrtc.org/experiments/rtp-hdrext/abs-send-time
+a=extmap:5 urn:ietf:params:rtp-hdrext:toffset
+a=extmap:6/recvonly http://www.webrtc.org/experiments/rtp-hdrext/playout-delay
+a=extmap:7 http://www.ietf.org/id/draft-holmer-rmcat-transport-wide-cc-extensions-01
+a=fmtp:126 profile-level-id=42e01f;level-asymmetry-allowed=1;packetization-mode=1
+a=fmtp:97 profile-level-id=42e01f;level-asymmetry-allowed=1
+a=fmtp:120 max-fs=12288;max-fr=60
+a=fmtp:124 apt=120
+a=fmtp:121 max-fs=12288;max-fr=60
+a=fmtp:125 apt=121
+a=fmtp:127 apt=126
+a=fmtp:98 apt=97
+a=ice-pwd:e2f571f28334aa91043ed38190223da9
+a=ice-ufrag:9dfb71ef
+a=mid:0
+a=rtcp-fb:121 nack
+a=rtcp-fb:121 nack pli
+a=rtcp-fb:121 ccm fir
+a=rtcp-fb:121 goog-remb
+a=rtcp-fb:121 transport-cc
+a=rtcp-fb:126 nack
+a=rtcp-fb:126 nack pli
+a=rtcp-fb:126 ccm fir
+a=rtcp-fb:126 goog-remb
+a=rtcp-fb:126 transport-cc
+a=rtcp-fb:97 nack
+a=rtcp-fb:97 nack pli
+a=rtcp-fb:97 ccm fir
+a=rtcp-fb:97 goog-remb
+a=rtcp-fb:97 transport-cc
+a=rtcp-mux
+a=rtcp-rsize
+a=rtpmap:124 rtx/90000
+a=rtpmap:121 VP9/90000
+a=rtpmap:125 rtx/90000
+a=rtpmap:126 H264/90000
+a=rtpmap:127 rtx/90000
+a=rtpmap:97 H264/90000
+a=rtpmap:98 rtx/90000
+a=setup:actpass
+a=ssrc:1644235696 cname:{36a6a74c-73a4-594b-9bb0-023b4d357280})";
+
+    offer += sdpaudio_sendrecv_mid0;
+    offer += "\n";
+    offer += sdpOfferAudio1;
+    offer += "\n";
+    offer += sdpOfferVideo1;
+    offer += "\n";
+    offer += sdpvideo;
+    offer += "\n";
+    offer += sdpOfferVideo2;
+    offer += "\n";
+
+    assertLFAndCRLF((PCHAR) offer.c_str(), offer.size(), [](PCHAR sdp) {
+        RtcConfiguration configuration{};
+        PRtcPeerConnection pRtcPeerConnection = nullptr;
+        RtcMediaStreamTrack track1{};
+        PRtcRtpTransceiver transceiver1 = nullptr;
+        RtcSessionDescriptionInit offerSdp{};
+        RtcSessionDescriptionInit answerSdp{};
+
+        SNPRINTF(configuration.iceServers[0].urls, MAX_ICE_CONFIG_URI_LEN, KINESIS_VIDEO_STUN_URL, TEST_DEFAULT_REGION);
+
+        track1.kind = MEDIA_STREAM_TRACK_KIND_VIDEO;
+        track1.codec = RTC_CODEC_VP8;
+        STRNCPY(track1.streamId, "stream1", MAX_MEDIA_STREAM_ID_LEN);
+        STRNCPY(track1.trackId, "track1", MAX_MEDIA_STREAM_TRACK_ID_LEN);
+
+        offerSdp.type = SDP_TYPE_OFFER;
+        STRNCPY(offerSdp.sdp, (PCHAR) sdp, MAX_SESSION_DESCRIPTION_INIT_SDP_LEN);
+
+        EXPECT_EQ(STATUS_SUCCESS, createPeerConnection(&configuration, &pRtcPeerConnection));
+        EXPECT_EQ(STATUS_SUCCESS, addSupportedCodec(pRtcPeerConnection, RTC_CODEC_VP8));
+        EXPECT_EQ(STATUS_SUCCESS, addTransceiver(pRtcPeerConnection, &track1, nullptr, &transceiver1));
+
+        EXPECT_EQ(STATUS_SUCCESS, setRemoteDescription(pRtcPeerConnection, &offerSdp));
+        EXPECT_EQ(TRUE, canTrickleIceCandidates(pRtcPeerConnection).value);
+        EXPECT_EQ(STATUS_SUCCESS, createAnswer(pRtcPeerConnection, &answerSdp));
+
+        std::string answer = answerSdp.sdp;
+
+        // check m-line and codec value
+        std::string::size_type posMline1 = answer.find("m=audio 9 UDP/TLS/RTP/SAVPF 111");
+        EXPECT_NE(std::string::npos, posMline1);
+        posMline1++;
+        std::string::size_type posMline2 = answer.find("m=audio 9 UDP/TLS/RTP/SAVPF 109", posMline1);
+        EXPECT_NE(std::string::npos, posMline2);
+        posMline2++;
+        std::string::size_type posMline3 = answer.find("m=video 9 UDP/TLS/RTP/SAVPF 97", posMline2);
+        EXPECT_NE(std::string::npos, posMline3);
+        posMline3++;
+        std::string::size_type posMline4 = answer.find("m=video 9 UDP/TLS/RTP/SAVPF 96", posMline3);
+        EXPECT_NE(std::string::npos, posMline4);
+        posMline4++;
+        std::string::size_type posMline5 = answer.find("m=video 9 UDP/TLS/RTP/SAVPF 124", posMline4);
+        EXPECT_NE(std::string::npos, posMline5);
+
+        // check order of the m-lines
+        EXPECT_EQ(TRUE, posMline1 < posMline2);
+        EXPECT_EQ(TRUE, posMline2 < posMline3);
+        EXPECT_EQ(TRUE, posMline3 < posMline4);
+        EXPECT_EQ(TRUE, posMline4 < posMline5);
+
+        // check directions
+        EXPECT_NE(std::string::npos, answer.substr(posMline1, posMline2 - posMline1).find("a=inactive"));
+        EXPECT_NE(std::string::npos, answer.substr(posMline2, posMline3 - posMline2).find("a=inactive"));
+        EXPECT_NE(std::string::npos, answer.substr(posMline3, posMline4 - posMline3).find("a=inactive"));
+        EXPECT_NE(std::string::npos, answer.substr(posMline4, posMline5 - posMline4).find("a=sendonly"));
+        EXPECT_NE(std::string::npos, answer.substr(posMline5).find("a=inactive"));
+
+        // check rtpmap values
+        EXPECT_NE(std::string::npos, answer.substr(posMline1, posMline2 - posMline1).find("a=rtpmap:111 opus/48000/2"));
+        EXPECT_NE(std::string::npos, answer.substr(posMline2, posMline3 - posMline2).find("a=rtpmap:109 opus/48000/2"));
+        EXPECT_NE(std::string::npos, answer.substr(posMline3, posMline4 - posMline3).find("a=rtpmap:97 rtx/90000"));
+        EXPECT_NE(std::string::npos, answer.substr(posMline5).find("a=rtpmap:124 rtx/90000"));
+
+        closePeerConnection(pRtcPeerConnection);
+        EXPECT_EQ(STATUS_SUCCESS, freePeerConnection(&pRtcPeerConnection));
+    });
+}
+
 TEST_P(SdpApiTest_SdpMatch, populateSingleMediaSection_TestH264Fmtp)
 {
     PRtcPeerConnection pRtcPeerConnection = NULL;
