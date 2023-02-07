734f9bf51914e97ab5e5cf3b959d84701f4b9df4
Niyati Maheshwari <niyatim23@gmail.com>
2023-02-06 17:28:55 -0800

    specify path to openssl root folder

--- .github/build_windows.bat
+++ .github/build_windows.bat
@@ -1,6 +1,6 @@
 call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
 mkdir build
 cd build
-cmd.exe /c cmake -G "NMake Makefiles" ..
-cmake -G "NMake Makefiles" -DBUILD_TEST=TRUE -DBUILD_OPENSSL_PLATFORM=x86_amd64 ..
+cmd.exe /c cmake -G "NMake Makefiles" -DOPENSSL_ROOT_DIR=D:\a\amazon-kinesis-video-streams-webrtc-sdk-c\amazon-kinesis-video-streams-webrtc-sdk-c\open-source\libopenssl\ ..
+cmake -G "NMake Makefiles" -DBUILD_TEST=TRUE -DOPENSSL_ROOT_DIR=D:\a\amazon-kinesis-video-streams-webrtc-sdk-c\amazon-kinesis-video-streams-webrtc-sdk-c\open-source\libopenssl\ ..
 nmake
