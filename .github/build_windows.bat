718e83fef7681f76531a839f71ae790f4ebb662b
Niyati Maheshwari <niyatim23@gmail.com>
2023-02-07 14:14:56 -0800

    update checkoutv2 to v3

--- .github/build_windows.bat
+++ .github/build_windows.bat
@@ -1,6 +1,6 @@
 call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
 mkdir build
 cd build
-cmd.exe /c cmake -G "MinGW Makefiles" ..
-cmake -G "MinGW Makefiles" -DBUILD_TEST=TRUE ..
+cmd.exe /c cmake -G "NMake Makefiles" ..
+cmake -G "NMake Makefiles" -DBUILD_TEST=TRUE ..
 nmake
