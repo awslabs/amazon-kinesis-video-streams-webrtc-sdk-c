6e40c4d76ba9028787b8113fb538b581b125e5fc
Niyati Maheshwari <niyatim@amazon.com>
2023-03-09 19:58:21 -0800

    pthreads

--- .github/build_windows.bat
+++ .github/build_windows.bat
@@ -2,5 +2,5 @@ call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Buil
 mkdir build
 cd build
 cmd.exe /c cmake -G "NMake Makefiles" ..
-cmake -G "NMake Makefiles" -DBUILD_TEST=TRUE -DLWS_WITH_THREADPOOL=ON -DLWS_HAVE_PTHREAD_H=1 -DLWS_EXT_PTHREAD_INCLUDE_DIR="C:\tools\pthreads-w64-2-10-0-release\Pre-built.2\include" -DLWS_EXT_PTHREAD_LIBRARIES="C:\tools\pthreads-w64-2-10-0-release\Pre-built.2\lib\x64\libpthreadGC2.a" -DLWS_WITH_MINIMAL_EXAMPLES=1 ..
+cmake -G "NMake Makefiles" -DBUILD_TEST=TRUE -DLWS_WITH_THREADPOOL=ON -DLWS_HAVE_PTHREAD_H=1 -DLWS_EXT_PTHREAD_INCLUDE_DIR="C:\tools\pthreads-w32-2-9-1-release\Pre-built.2\include" -DLWS_EXT_PTHREAD_LIBRARIES="C:\tools\pthreads-w32-2-9-1-release\Pre-built.2\lib\x64\libpthreadGC2.a" -DLWS_WITH_MINIMAL_EXAMPLES=1 ..
 nmake
