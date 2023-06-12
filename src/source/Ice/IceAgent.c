20758fa46e3a63dff61e805cbb9a8f65f8d6c98a
chenln1124 <112364884+chenln1124@users.noreply.github.com>
2023-06-12 14:10:48 +0800

    IoT CertificateId as Stream Name. (#1721)
    * IoT CertificateId as Stream Name.
    
    * Fix the clang-format use ./scripts/clang-format.sh -d src/
    
    ---------
    
    Co-authored-by: Lina Chen <chenln1124@thundersoft.com>

--- src/source/Ice/IceAgent.c
+++ src/source/Ice/IceAgent.c
@@ -2641,7 +2641,7 @@ STATUS iceAgentCheckPeerReflexiveCandidate(PIceAgent pIceAgent, PKvsIpAddress pI
     CHK_STATUS(doubleListGetNodeCount(pIceAgent->remoteCandidates, &candidateCount));
     CHK_WARN(candidateCount < KVS_ICE_MAX_REMOTE_CANDIDATE_COUNT, retStatus, "max remote candidate count exceeded"); // return early if limit exceeded
     CHK_STATUS(findCandidateWithIp(pIpAddress, pIceAgent->remoteCandidates, &pIceCandidate));
-    CHK(pIceCandidate == NULL, retStatus);                                                                           // return early if duplicated
+    CHK(pIceCandidate == NULL, retStatus); // return early if duplicated
     DLOGD("New remote peer reflexive candidate found");
 
     CHK((pIceCandidate = MEMCALLOC(1, SIZEOF(IceCandidate))) != NULL, STATUS_NOT_ENOUGH_MEMORY);
