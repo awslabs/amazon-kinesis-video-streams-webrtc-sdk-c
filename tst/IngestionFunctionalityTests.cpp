665bf357fe96ab6947c066f928fd68efcf1ec287
Divya Sampath Kumar <disa6302@colorado.edu>
2023-12-27 11:46:28 -0800

    Uncomment a few more lines

--- tst/IngestionFunctionalityTests.cpp
+++ tst/IngestionFunctionalityTests.cpp
@@ -323,15 +323,15 @@ TEST_F(IngestionFunctionalityTest, basicCreateConnectJoinSession)
     signalingClientMetrics.version = SIGNALING_CLIENT_METRICS_CURRENT_VERSION;
 
     MediaConfigurationInfo mediaConfigurationInfo = createStreamAndChannelAndLink();
-//    ASSERT_EQ(TRUE, mediaConfigurationInfo.isValid);
-//    ASSERT_EQ(TRUE, mediaConfigurationInfo.enabledStatus);
-//
-//
-//    ChannelInfo channelInfo;
-//    SignalingClientCallbacks signalingClientCallbacks;
-//    SignalingClientInfoInternal clientInfoInternal;
-//    SIGNALING_CLIENT_HANDLE signalingHandle = INVALID_SIGNALING_CLIENT_HANDLE_VALUE;
-//    PSignalingClient pSignalingClient;
+    ASSERT_EQ(TRUE, mediaConfigurationInfo.isValid);
+    ASSERT_EQ(TRUE, mediaConfigurationInfo.enabledStatus);
+
+
+    ChannelInfo channelInfo;
+    SignalingClientCallbacks signalingClientCallbacks;
+    SignalingClientInfoInternal clientInfoInternal;
+    SIGNALING_CLIENT_HANDLE signalingHandle = INVALID_SIGNALING_CLIENT_HANDLE_VALUE;
+    PSignalingClient pSignalingClient;
 //
 //    signalingClientCallbacks.version = SIGNALING_CLIENT_CALLBACKS_CURRENT_VERSION;
 //    signalingClientCallbacks.customData = (UINT64) this;
