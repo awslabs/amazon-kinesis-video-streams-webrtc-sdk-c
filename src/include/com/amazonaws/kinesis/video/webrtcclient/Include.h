8ef12b9de5443e0d7e99e1058c40f174085fee72
Hongli Wang <hongliwo@amazon.com>
2024-03-09 02:23:49 +0800

    Add an enumeration of RTC_CODEC_H265=6 in RTC_CODEC

--- src/include/com/amazonaws/kinesis/video/webrtcclient/Include.h
+++ src/include/com/amazonaws/kinesis/video/webrtcclient/Include.h
@@ -851,7 +851,9 @@ typedef enum {
     RTC_CODEC_VP8 = 3,                                                            //!< VP8 video codec.
     RTC_CODEC_MULAW = 4,                                                          //!< MULAW audio codec
     RTC_CODEC_ALAW = 5,                                                           //!< ALAW audio codec
-    RTC_CODEC_UNKNOWN = 6,
+    RTC_CODEC_H265 = 6,                                                           //!< H265 video codec
+    RTC_CODEC_UNKNOWN = 7,
+
     // RTC_CODEC_MAX **MUST** be the last enum in the list **ALWAYS** and not assigned a value
     RTC_CODEC_MAX //!< Placeholder for max number of supported codecs
 } RTC_CODEC;
