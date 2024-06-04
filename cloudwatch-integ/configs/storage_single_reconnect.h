a2918678fed7e695f70846c670918034ef4e2377
Divya Sampath Kumar <disa6302@colorado.edu>
2024-06-04 13:50:51 -0700

    Update config

--- cloudwatch-integ/configs/storage_single_reconnect.h
+++ cloudwatch-integ/configs/storage_single_reconnect.h
@@ -1,8 +1,20 @@
-//
-// Created by Sampath Kumar, Divya on 5/29/24.
-//
+#ifndef KVS_SDK_SAMPLE_CONFIG_H
+#define KVS_SDK_SAMPLE_CONFIG_H
 
-#ifndef KVS_SDK_STORAGE_SINGLE_RECONNECT_H
-#define KVS_SDK_STORAGE_SINGLE_RECONNECT_H
-
-#endif //KVS_SDK_STORAGE_SINGLE_RECONNECT_H
+#define USE_TRICKLE_ICE             TRUE
+#define FORCE_TURN_ONLY             FALSE
+#define RUNNER_LABEL                (PCHAR) "StoragePeriodic"
+#define SCENARIO_LABEL              (PCHAR) "StoragePeriodic"
+#define USE_TURN                    TRUE
+#define ENABLE_TTFF_VIA_DC          FALSE
+#define USE_IOT                     FALSE
+#define ENABLE_STORAGE              TRUE
+#define ENABLE_METRICS              TRUE
+#define SAMPLE_PRE_GENERATE_CERT    TRUE
+#define RUN_TIME                    (3900 * HUNDREDS_OF_NANOS_IN_A_SECOND)
+#define LOG_GROUP_NAME              (PCHAR) "WebrtcSDK"
+#define CHANNEL_NAME_PREFIX         (PCHAR) "DEFAULT"
+#define USE_STORAGE                 TRUE
+#define AUDIO_CODEC                 RTC_CODEC_OPUS
+#define VIDEO_CODEC                 RTC_CODEC_H264_PROFILE_42E01F_LEVEL_ASYMMETRY_ALLOWED_PACKETIZATION_MODE
+#endif // KVS_SDK_SAMPLE_CONFIG_H
