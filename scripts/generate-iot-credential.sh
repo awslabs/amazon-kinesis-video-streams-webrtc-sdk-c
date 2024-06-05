0eb9d47cfe16bb3da144d5a476ec1f2b404532da
Divya Sampath Kumar <disa6302@colorado.edu>
2024-06-05 13:56:00 -0700

    Scripts

--- scripts/generate-iot-credential.sh
+++ scripts/generate-iot-credential.sh
@@ -2,15 +2,17 @@
 # You need to setup your aws cli first, because this script is based on aws cli.
 # You can use this script to setup environment variables in the shell which samples run on.
 # https://docs.aws.amazon.com/kinesisvideostreams/latest/dg/how-iot.html
-thingName="webrtc_iot_thing"
-thingTypeName="webrtc_iot_thing_type"
-iotPolicyName="webrtc_iot_policy"
-kvsPolicyName="webrtc_policy"
-iotRoleName="webrtc_iot_role"
-iotRoleAlias="webrtc_iot_role_alias"
-iotCert="webrtc_iot_certifcate.pem"
-iotPublicKey="webrtc_iot_public.key"
-iotPrivateKey="webrtc_iot_private.key"
+
+prefix=$1
+thingName="${prefix}_thing"
+thingTypeName="${prefix}_thing_type"
+iotPolicyName="${prefix}_policy"
+kvsPolicyName="${prefix}_policy"
+iotRoleName="${prefix}_role"
+iotRoleAlias="${prefix}_role_alias"
+iotCert="${prefix}_certificate.pem"
+iotPublicKey="${prefix}_public.key"
+iotPrivateKey="${prefix}_private.key"
 
 # Step 1: Create an IoT Thing Type and an IoT Thing
 # The following example command creates a thing type $thingTypeName
@@ -65,18 +67,18 @@ cat > iot-policy-document.json <<EOF
    "Version":"2012-10-17",
    "Statement":[
       {
-	 "Effect":"Allow",
-	 "Action":[
-	    "iot:Connect"
-	 ],
-	 "Resource":"$(jq --raw-output '.roleAliasArn' iot-role-alias.json)"
+     "Effect":"Allow",
+     "Action":[
+        "iot:Connect"
+     ],
+     "Resource":"$(jq --raw-output '.roleAliasArn' iot-role-alias.json)"
  },
       {
-	 "Effect":"Allow",
-	 "Action":[
-	    "iot:AssumeRoleWithCertificate"
-	 ],
-	 "Resource":"$(jq --raw-output '.roleAliasArn' iot-role-alias.json)"
+     "Effect":"Allow",
+     "Action":[
+        "iot:AssumeRoleWithCertificate"
+     ],
+     "Resource":"$(jq --raw-output '.roleAliasArn' iot-role-alias.json)"
  }
    ]
 }
@@ -86,22 +88,14 @@ aws --profile default iot create-policy --policy-name $iotPolicyName --policy-do
 
 # Step 3: Create and Configure the X.509 Certificate
 # Create the certificate to which you must attach the policy for IoT that you created above.
-aws --profile default  iot create-keys-and-certificate --set-as-active --certificate-pem-outfile $iotCert --public-key-outfile $iotPublicKey --private-key-outfile $iotPrivateKey > certificate
-# Attach the policy for IoT (KvsCameraIoTPolicy created above) to this certificate.
-aws --profile default  iot attach-policy --policy-name $iotPolicyName --target $(jq --raw-output '.certificateArn' certificate)
-# Attach your IoT thing (kvs_example_camera_stream) to the certificate you just created:
-aws --profile default  iot attach-thing-principal --thing-name $thingName --principal $(jq --raw-output '.certificateArn' certificate)
-# In order to authorize requests through the IoT credentials provider, you need the IoT credentials endpoint which is unique to your AWS account ID. You can use the following command to get the IoT credentials endpoint.
-aws --profile default  iot describe-endpoint --endpoint-type iot:CredentialProvider --output text > iot-credential-provider.txt
-# In addition to the X.509 cerficiate created above, you must also have a CA certificate to establish trust with the back-end service through TLS. You can get the CA certificate using the following command:
-curl --silent 'https://www.amazontrust.com/repository/SFSRootCAG2.pem' --output cacert.pem
-
-
-export AWS_IOT_CORE_CREDENTIAL_ENDPOINT=$(cat iot-credential-provider.txt)
-export AWS_IOT_CORE_CERT=$(pwd)"/"$iotCert
-export AWS_IOT_CORE_PRIVATE_KEY=$(pwd)"/"$iotPrivateKey
-export AWS_IOT_CORE_ROLE_ALIAS=$iotRoleAlias
-export AWS_IOT_CORE_THING_NAME=$thingName
-        
+# aws --profile default  iot create-keys-and-certificate --set-as-active --certificate-pem-outfile $iotCert --public-key-outfile $iotPublicKey --private-key-outfile $iotPrivateKey > certificate
+# # Attach the policy for IoT (KvsCameraIoTPolicy created above) to this certificate.
+# aws --profile default  iot attach-policy --policy-name $iotPolicyName --target $(jq --raw-output '.certificateArn' certificate)
+# # Attach your IoT thing (kvs_example_camera_stream) to the certificate you just created:
+# aws --profile default  iot attach-thing-principal --thing-name $thingName --principal $(jq --raw-output '.certificateArn' certificate)
+# # In order to authorize requests through the IoT credentials provider, you need the IoT credentials endpoint which is unique to your AWS account ID. You can use the following command to get the IoT credentials endpoint.
+# aws --profile default  iot describe-endpoint --endpoint-type iot:CredentialProvider --output text > iot-credential-provider.txt
+# # In addition to the X.509 cerficiate created above, you must also have a CA certificate to establish trust with the back-end service through TLS. You can get the CA certificate using the following command:
+# curl --silent 'https://www.amazontrust.com/repository/SFSRootCAG2.pem' --output cacert.pem
 
 
