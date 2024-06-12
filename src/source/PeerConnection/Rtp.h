1589f0bc1c3e02e25b212fa02816313ebe1efc71
Divya Sampath Kumar <disa6302@colorado.edu>
2024-06-12 07:57:45 -0700

    add max values

--- src/source/PeerConnection/Rtp.h
+++ src/source/PeerConnection/Rtp.h
@@ -10,16 +10,18 @@ extern "C" {
 // Default MTU comes from libwebrtc
 // https://groups.google.com/forum/#!topic/discuss-webrtc/gH5ysR3SoZI
 #define DEFAULT_MTU_SIZE_BYTES                     1200
-#define DEFAULT_ROLLING_BUFFER_DURATION_IN_SECONDS 3
-#define DEFAULT_EXPECTED_VIDEO_BIT_RATE            (5 * 1024 * 1024)
+#define DEFAULT_ROLLING_BUFFER_DURATION_IN_SECONDS (DOUBLE) 3
+#define DEFAULT_EXPECTED_VIDEO_BIT_RATE            (DOUBLE) (5 * 1024 * 1024)
 // Opus has highest based on the supported codecs which is 510Kibps. So setting this to twice
-#define DEFAULT_EXPECTED_AUDIO_BIT_RATE        (1000 * 1024)
+#define DEFAULT_EXPECTED_AUDIO_BIT_RATE        (DOUBLE) (1000 * 1024)
 #define DEFAULT_SEQ_NUM_BUFFER_SIZE            1000
 #define DEFAULT_VALID_INDEX_BUFFER_SIZE        1000
 #define DEFAULT_PEER_FRAME_BUFFER_SIZE         (5 * 1024)
 #define SRTP_AUTH_TAG_OVERHEAD                 10
 #define MIN_ROLLING_BUFFER_DURATION_IN_SECONDS (DOUBLE) 0.1
 #define MIN_EXPECTED_BIT_RATE                  (DOUBLE)(102.4 * 1024) // Considering 1Kib = 1024 bits
+#define MAX_ROLLING_BUFFER_DURATION_IN_SECONDS (DOUBLE) 10.0
+#define MAX_EXPECTED_BIT_RATE                  (DOUBLE)(240 * 1024 * 1024) // Considering 1Kib = 1024 bits
 
 // https://www.w3.org/TR/webrtc-stats/#dom-rtcoutboundrtpstreamstats-huge
 // Huge frames, by definition, are frames that have an encoded size at least 2.5 times the average size of the frames.
