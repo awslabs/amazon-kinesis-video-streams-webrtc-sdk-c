6da7514cd3938f4ea536a23d33cd68f4b8c0102c
Incense <68001909+AnasIncense@users.noreply.github.com>
2025-05-17 02:28:52 +0800

    Fix H265 FMTP issue with kvsWebrtcClientViewer (#2138)
    Fix H265 FMTP issue with kvsWebrtcClientViewer

--- src/source/PeerConnection/SessionDescription.h
+++ src/source/PeerConnection/SessionDescription.h
@@ -51,10 +51,8 @@ extern "C" {
 #define DEFAULT_PAYLOAD_MULAW_STR (PCHAR) "0"
 #define DEFAULT_PAYLOAD_ALAW_STR  (PCHAR) "8"
 
-#define DEFAULT_H264_FMTP (PCHAR) "level-asymmetry-allowed=1;packetization-mode=1;profile-level-id=42e01f"
-#define DEFAULT_H265_FMTP                                                                                                                            \
-    (PCHAR) "profile-space=0;profile-id=0;tier-flag=0;level-id=0;interop-constraints=000000000000;sprop-vps=QAEMAf//"                                \
-            "AIAAAAMAAAMAAAMAAAMAALUCQA==;sprop-sps=QgEBAIAAAAMAAAMAAAMAAAMAAKACgIAtH+W1kkbQzkkktySqSfKSyA==;sprop-pps=RAHBpVgeSA=="
+#define DEFAULT_H264_FMTP   (PCHAR) "level-asymmetry-allowed=1;packetization-mode=1;profile-level-id=42e01f"
+#define DEFAULT_H265_FMTP   (PCHAR) "level-id=93;profile-id=1"
 #define DEFAULT_OPUS_FMTP   (PCHAR) "minptime=10;useinbandfec=1"
 #define H264_PROFILE_42E01F 0x42e01f
 // profile-level-id:
