c8e2ab7ed88b00ca8b8823ddf310523e322f496e
YPandas <125420341+YPandas@users.noreply.github.com>
2026-03-30 18:35:29 +0000

    Deploying to gh-pages from @ awslabs/amazon-kinesis-video-streams-webrtc-sdk-c@846c390e882a8fae2be794cb42e9894df241e41a 🚀

--- search/functions_7.js
+++ search/functions_7.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['handledceppacket_0',['handleDcepPacket',['../Sctp_8c.html#a8f12cbc92f7a361cec205992aba52480',1,'Sctp.c']]],
-  ['handlestunpacket_1',['handlestunpacket',['../IceAgent_8c.html#a15dca4483895fee272df46d7e4a955d7',1,'handleStunPacket(PIceAgent pIceAgent, PBYTE pBuffer, UINT32 bufferLen, PSocketConnection pSocketConnection, PKvsIpAddress pSrcAddr, PKvsIpAddress pDestAddr):&#160;IceAgent.c'],['../IceAgent_8h.html#ac05b87e31e89363944974da428845ef3',1,'handleStunPacket(PIceAgent, PBYTE, UINT32, PSocketConnection, PKvsIpAddress, PKvsIpAddress):&#160;IceAgent.c']]],
+  ['handlestunpacket_1',['handlestunpacket',['../IceAgent_8c.html#a6c1328530aa9bf24d4e8869be7334567',1,'handleStunPacket(PIceAgent pIceAgent, PBYTE pBuffer, UINT32 bufferLen, PSocketConnection pSocketConnection, PKvsIpAddress pSrcAddr, PKvsIpAddress pDestAddr, PSocketConnection *ppSocketConnectionToShutdown):&#160;IceAgent.c'],['../IceAgent_8h.html#aa75ae069a4e7cf0dfc7a0b6f71b827e6',1,'handleStunPacket(PIceAgent, PBYTE, UINT32, PSocketConnection, PKvsIpAddress, PKvsIpAddress, PSocketConnection *):&#160;IceAgent.c']]],
   ['hastransceiverwithssrc_2',['hastransceiverwithssrc',['../Rtp_8c.html#a3f98236c016e0072d6ef135477ffa895',1,'hasTransceiverWithSsrc(PKvsPeerConnection pKvsPeerConnection, UINT32 ssrc):&#160;Rtp.c'],['../Rtp_8h.html#a3f98236c016e0072d6ef135477ffa895',1,'hasTransceiverWithSsrc(PKvsPeerConnection pKvsPeerConnection, UINT32 ssrc):&#160;Rtp.c']]],
   ['headcheckingallowed_3',['headCheckingAllowed',['../JitterBuffer_8c.html#a91d568e855ef30dbee0e36a55c19d5c6',1,'JitterBuffer.c']]],
   ['headsequencenumbercheck_4',['headSequenceNumberCheck',['../JitterBuffer_8c.html#a6eabe7d0056f5acef21c7ff857c0fc78',1,'JitterBuffer.c']]],
