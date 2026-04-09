3fef1a0bc8e69387d352d861e3fabf1bab89a2ee
YPandas <125420341+YPandas@users.noreply.github.com>
2026-04-09 16:35:19 +0000

    Deploying to gh-pages from @ awslabs/amazon-kinesis-video-streams-webrtc-sdk-c@471f1e306de6b1c9dc8d3456f23bdab182514441 🚀

--- search/classes_3.js
+++ search/classes_3.js
@@ -3,5 +3,6 @@ var searchData=
   ['dscppacketssentrecord_0',['DscpPacketsSentRecord',['../structDscpPacketsSentRecord.html',1,'']]],
   ['dtlskeyingmaterial_1',['DtlsKeyingMaterial',['../structDtlsKeyingMaterial.html',1,'']]],
   ['dtlssessioncallbacks_2',['DtlsSessionCallbacks',['../structDtlsSessionCallbacks.html',1,'']]],
-  ['dualkvsipaddresses_3',['DualKvsIpAddresses',['../structDualKvsIpAddresses.html',1,'']]]
+  ['dtlssessionoptions_3',['DtlsSessionOptions',['../structDtlsSessionOptions.html',1,'']]],
+  ['dualkvsipaddresses_4',['DualKvsIpAddresses',['../structDualKvsIpAddresses.html',1,'']]]
 ];
