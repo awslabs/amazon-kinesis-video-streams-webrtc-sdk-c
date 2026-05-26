e6f0cf60d9f9280d1e3c4686153623483e9063a4
sirknightj <sirknightj@gmail.com>
2026-05-26 15:21:20 +0000

    Deploying to gh-pages from @ awslabs/amazon-kinesis-video-streams-webrtc-sdk-c@356288a4f6db3ac483ba78af1deb0970b66f2a6f 🚀

--- search/classes_2.js
+++ search/classes_2.js
@@ -1,6 +1,5 @@
 var searchData=
 [
   ['channelinfo_0',['ChannelInfo',['../structChannelInfo.html',1,'']]],
-  ['congestionctx_1',['CongestionCtx',['../structCongestionCtx.html',1,'']]],
-  ['connectionlistener_2',['ConnectionListener',['../structConnectionListener.html',1,'']]]
+  ['connectionlistener_1',['ConnectionListener',['../structConnectionListener.html',1,'']]]
 ];
