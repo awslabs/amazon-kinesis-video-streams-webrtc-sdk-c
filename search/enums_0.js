18dc377469f4681473505759bf917cf66edcd77f
sirknightj <sirknightj@gmail.com>
2026-05-27 15:50:00 +0000

    Deploying to gh-pages from @ awslabs/amazon-kinesis-video-streams-webrtc-sdk-c@356288a4f6db3ac483ba78af1deb0970b66f2a6f 🚀

--- search/enums_0.js
+++ search/enums_0.js
@@ -1,6 +1,5 @@
 var searchData=
 [
   ['data_5fchannel_5ftype_0',['DATA_CHANNEL_TYPE',['../Sctp_8h.html#ac30615997d46d1cefff7c8755cd38792',1,'Sctp.h']]],
-  ['dtls_5fhandshake_5fstate_1',['DTLS_HANDSHAKE_STATE',['../Dtls_8h.html#a24a0a539af96854f8039db1b633a5fb8',1,'Dtls.h']]],
-  ['dtls_5fsession_5fvalidation_5fmode_2',['DTLS_SESSION_VALIDATION_MODE',['../Dtls_8h.html#ac74b5e481dbdea93ff7483fe26056719',1,'Dtls.h']]]
+  ['dtls_5fhandshake_5fstate_1',['DTLS_HANDSHAKE_STATE',['../Dtls_8h.html#a24a0a539af96854f8039db1b633a5fb8',1,'Dtls.h']]]
 ];
